d3381998ecdcad05589c0bb4ae251c938204adcd
SametEmsen <sametemsen@gmail.com>
2024-03-18 23:55:43 +0300

    Register feature added by SametEmsen

--- src/test/java/ourvirtualmarket/pages/BasePage.java
+++ src/test/java/ourvirtualmarket/pages/BasePage.java
@@ -1,10 +1,33 @@
 package ourvirtualmarket.pages;
 
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import ourvirtualmarket.utilities.Driver;
 
 public abstract class BasePage {
     public BasePage() {
         PageFactory.initElements(Driver.get(), this);
+    }
+    Faker faker=new Faker();
+
+
+    @FindBy(xpath = "//button[@class='popup-close']")
+    private WebElement subsPopupClose;
+    @FindBy(xpath = "//input[@name='hidden-popup']")
+    private WebElement subsCheckBox;
+    @FindBy(xpath = "//div[@class='signin-w font-title hidden-sm hidden-xs']//a[text()='Register']")
+    private WebElement registerBtn;
+
+    /**
+     * This method closes the "popup" on the main page.
+     */
+    public void closePopUp() {
+        subsCheckBox.click();
+        subsPopupClose.click();
+    }
+    public void navigateToRegisterPage(){
+        registerBtn.click();
     }
 }
